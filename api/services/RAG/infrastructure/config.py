416e7e5fe6af643a49a63ee022cbe5c0260c0654
Owen Adirah <owenadira@gmail.com>
2026-05-18 10:14:50 +0300

    feat(rag): authenticate requests with auth service
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)

--- api/services/RAG/infrastructure/config.py
+++ api/services/RAG/infrastructure/config.py
@@ -7,7 +7,9 @@ class Settings(BaseSettings):
     service_name: str = "rag"
     api_host: str = "0.0.0.0"
     api_port: int = 7000
-    allow_anonymous: bool = True
+    allow_anonymous: bool = False
+    auth_service_url: str = "http://auth-service:6000"
+    auth_request_timeout_seconds: float = 5.0
     file_storage_dir: str = "./var/files"
 
     model_config = SettingsConfigDict(env_prefix="RAG_", extra="ignore")
