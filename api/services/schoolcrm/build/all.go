453159ea11538782f55e6d0f52f08167da8ab4df
Owen Adirah <owenadira@gmail.com>
2026-05-29 16:13:14 +0300

    feat(admissions): wire duplicate audit bus

--- api/services/schoolcrm/build/all.go
+++ api/services/schoolcrm/build/all.go
@@ -41,6 +41,7 @@ func (all) Add(app *web.App, cfg mux.Config) {
 	admissionsapp.Routes(app, admissionsapp.Config{
 		Log:           cfg.Log,
 		AdmissionsBus: cfg.BusConfig.AdmissionsBus,
+		AuditBus:      cfg.BusConfig.AuditBus,
 		AuthClient:    cfg.SchoolCRMConfig.AuthClient,
 	})
 
