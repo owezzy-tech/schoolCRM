e9b2d45259c538aaadd24d31a2907026b9a5d09c
Owen Adirah <owenadira@gmail.com>
2026-05-30 03:01:32 +0300

    feat(web-admin): add users roles page

---
+++ api/frontends/web-admin/src/app/modules/admin/admin-users/admin-users.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { AdminUsersComponent } from 'app/modules/admin/admin-users/admin-users.component';
+
+export default [
+    {
+        path: '',
+        component: AdminUsersComponent,
+    },
+] as Routes;
