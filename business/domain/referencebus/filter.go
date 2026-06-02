d532c06f414173f1dfe63af5955b8bcf9ddde15e
Owen Adirah <owenadira@gmail.com>
2026-06-02 21:31:40 +0300

    feat(reference): define Kenya catalog models

--- business/domain/referencebus/filter.go
+++ business/domain/referencebus/filter.go
@@ -3,7 +3,11 @@ package referencebus
 // QueryFilter holds the available fields a query can be filtered on.
 // We are using pointer semantics because the With API mutates the value.
 type QueryFilter struct {
-	Code       *string
-	CountyCode *string
-	Name       *string
+	Code           *string
+	CountyCode     *string
+	SubCountyCode  *string
+	UniversityCode *string
+	ClusterCode    *string
+	KNQFLevelCode  *string
+	Name           *string
 }
